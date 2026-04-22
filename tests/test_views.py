c2ce8f8b61a4109d4f32dc4a5f044d5a4407162e
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-04-22 23:37:41 +0300

    Исправлены ошибки после проверки линтерами. Обновлен README.md

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,26 +6,35 @@ from src.views import main_page
 
 
 def test_main_page_valid_date():
+    """Тестирование возврата JSON-ответа"""
     date_input = "2020-05-20 12:00:00"
     result = main_page(date_input)
     assert json.loads(result)["greeting"] is not None
 
 
 def test_main_page_json_format():
+    """Тестирование возврата JSON-ответа"""
     date_input = "2020-05-20 12:00:00"
     result = main_page(date_input)
     assert json.loads(result) is not None
 
 
-@mock.patch('src.views.read_excel_file')
-@mock.patch('src.views.filter_by_date')
-@mock.patch('src.views.get_greeting')
-@mock.patch('src.views.get_cards')
-@mock.patch('src.views.get_top_trans')
-@mock.patch('src.views.get_currency_rates')
-@mock.patch('src.views.get_stock_prices')
-def test_main_page_function_calls(mock_get_stock_prices, mock_get_currency_rates, mock_get_top_trans, mock_get_cards,
-                                  mock_get_greeting, mock_filter_by_date, mock_read_excel_file):
+@mock.patch("src.views.read_excel_file")
+@mock.patch("src.views.filter_by_date")
+@mock.patch("src.views.get_greeting")
+@mock.patch("src.views.get_cards")
+@mock.patch("src.views.get_top_trans")
+@mock.patch("src.views.get_currency_rates")
+@mock.patch("src.views.get_stock_prices")
+def test_main_page_function_calls(
+    mock_get_stock_prices,
+    mock_get_currency_rates,
+    mock_get_top_trans,
+    mock_get_cards,
+    mock_get_greeting,
+    mock_filter_by_date,
+    mock_read_excel_file,
+):
     date_input = "2020-05-20 12:00:00"
 
     mock_read_excel_file.return_value = {}
@@ -39,7 +48,9 @@ def test_main_page_function_calls(mock_get_stock_prices, mock_get_currency_rates
     main_page(date_input)
 
     mock_read_excel_file.assert_called_once_with(config.EXCEL_DATA)
-    mock_filter_by_date.assert_called_once_with(mock_read_excel_file.return_value, date_input)
+    mock_filter_by_date.assert_called_once_with(
+        mock_read_excel_file.return_value, date_input
+    )
     mock_get_greeting.assert_called_once_with(date_input)
     mock_get_cards.assert_called_once_with(mock_filter_by_date.return_value)
     mock_get_top_trans.assert_called_once_with(mock_filter_by_date.return_value)
